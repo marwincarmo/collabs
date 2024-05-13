f759f420b8c6f2c4cb1ef2572aed3a39d70b6e87
Marwin Carmo <marwinbr@gmail.com>
2024-05-13 13:58:51 -0700

    explore missing data

--- concussion_uk/scripts/01_data-prep.R
+++ concussion_uk/scripts/01_data-prep.R
@@ -5,10 +5,29 @@ library(dplyr)
 
 # 01 Load in data ---------------------------------------------------------
 
-raw_data <- haven::read_sav("data/PCSNetworkModelAnalysis.sav")
+raw_data <- haven::read_sav("data/PCSNetworkModelAnalysis.sav") |> 
+  janitor::clean_names()
 
 dplyr::glimpse(raw_data)
 
+
+# 02 Check for missing data -----------------------------------------------
+
+symptoms <- names(raw_data)[19:length(names(raw_data))]
+
+long_data <- raw_data |> 
+  dplyr::select(c(subject_id, survey_time, survey_day, head:vis)) |> 
+  tidyr::pivot_longer(cols =head:vis,
+                      names_to = "symptom",
+                      values_to = "score")
+long_data |> 
+  dplyr::with_groups(symptom,
+                     dplyr::summarize,
+                       n_missing = sum(is.na(score))
+                     ) |> 
+  print(n = Inf)
+
+# are the missing observations already deleted?
 
 # 02 Create between persons data ------------------------------------------
 
