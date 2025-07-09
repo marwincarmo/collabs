1d80cca2e2734b83bcbb793494502d478e98a67e
Marwin Carmo <marwinbr@gmail.com>
2025-07-08 17:30:37 -0700

    use more variables

--- concussion_uk/scripts/project1.R
+++ concussion_uk/scripts/project1.R
@@ -6,10 +6,11 @@ library(imputeTS)
 library(dplyr)
 library(ggplot2)
 library(stringr)
+library(qgraph)
 
 # 1 Load in data ----------------------------------------------------------
 
-d <- readRDS("data/SynthData.rds")
+d <- readRDS("data/SynthData14.rds")
 head(d$syn)
 
 # 2 Select the most central symptoms --------------------------------------
@@ -44,42 +45,23 @@ sxd_imp <- sxd |>
                 .names = "{.col}_imp")) |> 
   ungroup() |> 
   select(Main.SubjectIDNum, obs_num, contains("_imp")) |>
-  rename_with(~str_remove(.x, "_imp"), contains("_imp")) |> 
+  rename_with(~str_remove(.x, "_imp"), contains("_imp")) |>
   mutate(id = as.numeric(as.factor(Main.SubjectIDNum)))
 
-
-
-
-
+## 3.2 Standardize the data
 
 sxd_imp_std <- sxd_imp |> 
   dplyr::mutate(
-    dplyr::across(Headache:DontFeelRight, scale)
+    dplyr::across(Headache:FatigueOrLowEnergy, scale)
   )
-
-sxd_imp %>%
-  group_by(Main.SubjectIDNum) %>%
-  summarize(has_duplicates = any(duplicated(obs_num))) |> 
-  dplyr::pull(has_duplicates) |> 
-  sum()
-
-## Look for zero variance
-sxd_imp |> 
-  group_by(Main.SubjectIDNum) |> 
-  summarise(across(everything(), sd)) |> 
-  filter(if_any(everything(), ~ . == 0 | is.na(.)))
-  print(n = Inf)
-
-  sxd_imp <- sxd_imp |> 
-    dplyr::filter(!Main.SubjectIDNum %in% c(576, 853, 2586))
 
 # 4 mlVAR -----------------------------------------------------------------
 
-vars <- colnames(sxd_imp[,-c(1,2)])
-sxd_imp$obs_num <- 
 
-fit_mlvar <- mlVAR(data=sxd_imp, vars = vars, idvar = "id",
-                   contemporaneous = "orthogonal", temporal = "orthogonal", estimator = "lmer")
+vars <- colnames(sxd_imp[,3:9])
+
+fit_mlvar <- mlVAR(data=sxd_imp, vars = vars, idvar = "Main.SubjectIDNum",
+                   contemporaneous = "orthogonal", temporal = "orthogonal", estimator = "lmer", scale = TRUE)
 
 summary(fit_mlvar)
 
@@ -173,54 +155,189 @@ centrality_mlvar <- function(fit,
 centrality_res <- centrality_mlvar(fit_mlvar)
 
 
+#
+# Function to summarize the most frequent central symptom
+freq_central <- function(centrality_output, var_names) {
+  
+  # --- Temporal Network (Outstrength and Instrength) ---
+  
+  # Most frequent central symptom based on outstrength
+  outstrength_ranks <- lapply(centrality_output$outstrength, function(x) {
+    names(x) <- var_names
+    order(x, decreasing = TRUE)
+  })
+  outstrength_most_central <- unlist(lapply(outstrength_ranks, function(x) var_names[x[1]]))
+  outstrength_summary <- as.data.frame(table(outstrength_most_central))
+  names(outstrength_summary) <- c("Symptom", "Frequency")
+  outstrength_summary$Proportion <- outstrength_summary$Frequency / sum(outstrength_summary$Frequency)
+  outstrength_summary <- outstrength_summary[order(outstrength_summary$Frequency, decreasing = TRUE), ]
+  
+  # Most frequent central symptom based on instrength
+  instrength_ranks <- lapply(centrality_output$instrength, function(x) {
+    names(x) <- var_names
+    order(x, decreasing = TRUE)
+  })
+  instrength_most_central <- unlist(lapply(instrength_ranks, function(x) var_names[x[1]]))
+  instrength_summary <- as.data.frame(table(instrength_most_central))
+  names(instrength_summary) <- c("Symptom", "Frequency")
+  instrength_summary$Proportion <- instrength_summary$Frequency / sum(instrength_summary$Frequency)
+  instrength_summary <- instrength_summary[order(instrength_summary$Frequency, decreasing = TRUE), ]
+  
+  
+  # --- Contemporaneous Network (Strength) ---
+  
+  # Most frequent central symptom based on strength
+  strength_ranks <- lapply(centrality_output$strength, function(x) {
+    names(x) <- var_names
+    order(x, decreasing = TRUE)
+  })
+  strength_most_central <- unlist(lapply(strength_ranks, function(x) var_names[x[1]]))
+  strength_summary <- as.data.frame(table(strength_most_central))
+  names(strength_summary) <- c("Symptom", "Frequency")
+  strength_summary$Proportion <- strength_summary$Frequency / sum(strength_summary$Frequency)
+  strength_summary <- strength_summary[order(strength_summary$Frequency, decreasing = TRUE), ]
+  
+  return(list(
+    temporal_outstrength_summary = outstrength_summary,
+    temporal_instrength_summary = instrength_summary,
+    contemporaneous_strength_summary = strength_summary
+  ))
+}
 
-# Diagnostics -------------------------------------------------------------
+## 5.1 Individual centrality indices ----
 
-library(dplyr)
+most_frequent_central <- freq_central(centrality_output = centrality_res, var_names = vars)
+most_frequent_central
 
-# Define the variables in the model
-vars_in_model <- vars
+## 5.2 Centrality of the fixed effects -----
 
-# Manually create a lagged dataset to see how many complete cases exist for a model
-usable_rows_count <- sxd_imp %>%
-  select(Main.SubjectIDNum, obs_num, all_of(vars_in_model)) %>%
-  arrange(Main.SubjectIDNum, obs_num) %>%
-  group_by(Main.SubjectIDNum) %>%
-  # Create lagged versions of all variables to simulate the predictors
-  mutate(across(all_of(vars_in_model), ~lag(.), .names = "{.col}_lag")) %>%
-  ungroup() %>%
-  # Drop the ID and time variables for counting
-  select(-Main.SubjectIDNum, -obs_num) %>%
-  # How many rows are complete across all original AND lagged variables?
-  filter(complete.cases(.)) %>%
-  nrow()
+cont <- getNet(fit_mlvar, "contemporaneous", layout = "spring", nonsig = "hide", rule = "and")
+temp <- getNet(fit_mlvar, "temporal", nonsig = "hide")
 
-cat("Total number of usable rows for the temporal models:", usable_rows_count, "\n")
+centrality_auto(cont)$node.centrality["Strength"]
+centrality_auto(temp)$node.centrality[c("InStrength", "OutStrength")]
 
-library(dplyr)
 
-# Define your study's structure
-beeps_per_day <- 3
-total_days <- 7
-required_obs <- beeps_per_day * total_days # Should be 21
+# 6 Plots -----------------------------------------------------------------
 
-# Create day/beep columns and filter for complete subjects
-sxd_filtered_complete <- sxd %>%
-  mutate(
-    day  = ((obs_num - 1) %/% beeps_per_day) + 1,
-    beep = ((obs_num - 1) %% beeps_per_day) + 1
-  ) %>%
-  group_by(Main.SubjectIDNum) %>%
-  filter(n() == required_obs) %>%
-  ungroup()
+L <- averageLayout(cont, temp)
 
-# --- Verification ---
-# You can check the results to see how many subjects were kept
+p_cont <- qgraph(cont, layout = L, negDashed=FALSE, legend.cex=0.4, details=FALSE,
+       legend=FALSE, theme = "colorblind")
+plot(p_cont)
 
-# Original number of subjects
-cat("Original number of subjects:", n_distinct(sxd$Main.SubjectIDNum), "\n")
+p_temp <- qgraph(temp, layout = L, negDashed=FALSE, legend.cex=0.4, details=FALSE,
+       legend=FALSE, theme = "colorblind")
+plot(p_temp)
 
-# Number of subjects with complete data (21 observations)
-cat("Subjects with complete data:", n_distinct(sxd_filtered_complete$Main.SubjectIDNum), "\n")
+## 6.1 Saving the plots as tiff
 
-plot(fit_mlvar, type = "temporal")
+tiff("plot_contemporaneous.tiff", res = 300, width = 1024, height = 768, compression = "lzw")
+plot(p_cont)
+dev.off()
+
+tiff("plot_temporal.tiff", res = 300, width = 1024, height = 768, compression = "lzw")
+plot(p_temp)
+dev.off()
+
+
+# 7 dcnet ------------------------------------------------------------------
+## library(dcnet)
+## install.packages("data.table")
+## library(data.table)
+
+## head(sxd)
+
+## N <- length(unique(sxd_imp_std$id))
+## N
+
+## ids <- unique(sxd_imp_std$id)
+## tl <- 14
+## groupvec <- rep(seq_len(N),  each = tl)
+## dt <- data.table::data.table(sxd_imp_std )
+## variables <- names(sxd_imp_std)[3:8]
+
+## tsdat <- lapply(ids, function(x) dt[dt$id == x, .SD, .SDcols = variables] )
+## tsdat
+
+## #devtools::load_all(path = "./")
+
+## fit_init <- dcnet(data = tsdat, J = N, group = groupvec, S_pred = NULL,
+##                   standardize_data = FALSE,
+##                   parameterization = "CCC",
+##                   iterations = 30000,
+##                   sampling_algorithm = "variational",
+##                   meanstructure = "VAR",
+##                   chains = 4,
+##                   eta=0.1,
+##                   threads = 8,
+##                   grainsize = 5,
+##                   init = 0)
+
+## fit_init$model_fit$output()
+## grep("vec_phi_fixed", colnames(fit_init$model_fit$draws()))
+## fit_init$model_fit$draws()[, 3062:3070]
+## variables
+## phi <- matrix(colMeans(fit_init$model_fit$draws()[, grep("vec_phi_fixed", colnames(fit_init$model_fit$draws()))]), ncol = length(variables))
+## round(phi, 4)
+## #pdf(file = "~/Nextcloud/Documents/eric.pdf")
+## qgraph::qgraph(phi, labels = variables)
+## #dev.off()
+
+## fit <- dcnet(
+##     data = tsdat, parameterization = "CCC", J = N,
+##     group = groupvec, standardize_data = FALSE,
+##     init = fit_init$model_fit,
+##     meanstructure = "VAR",
+##     iterations = 50000,
+##     sampling_algorithm = "variational",
+##     algorithm = "fullrank", ## fullrank should be less biased
+##     grad_samples = 1,
+##     elbo_samples = 150,
+##     eta = 0.1,
+##     adapt_iter = 100,
+##     grainsize = 40,
+##     chains = 4,
+##     threads_per_chain = 4)
+
+
+
+##     # Simulate data:
+##      Model <- mlVARsim(nPerson = 50, nNode = 3, nTime = 50, lag=1)
+##      Model$Data = Model$Data*10
+##      # Estimate using correlated random effects:
+##      fit1 <- mlVAR(Model$Data, vars = Model$vars, idvar = Model$idvar, lags = 1, temporal = "correlated")
+## summary(fit1)
+
+## N <- 50
+## N
+## ids <- unique(Model$Data$ID)/10
+## ids
+## tl <- 50
+## groupvec <- rep(seq_len(N),  each = tl)
+## groupvec
+## dt <- data.table::data.table(Model$Data )
+## dt$ID=dt$ID/10
+## dt
+## variables <- names(dt)[1:3]
+## variables
+
+## tsdat <- lapply(ids, function(x) dt[dt$ID == x, .SD, .SDcols = variables] )
+## tsdat
+
+## #devtools::load_all(path = "./")
+
+## fit_init <- dcnet(data = tsdat, J = N, group = groupvec, S_pred = NULL,
+##                   standardize_data = TRUE,
+##                   parameterization = "CCC",
+##                   iterations = 30000,
+##                   sampling_algorithm = "variational",
+##                   meanstructure = "VAR",
+##                   chains = 4,
+##                   eta=0.1,
+##                   threads = 8,
+##                   grainsize = 5,
+##                   init = 0)
+
+## phi <- matrix(colMeans(fit_init$model_fit$draws()[, grep("vec_phi_fixed", colnames(fit_init$model_fit$draws()))]), ncol = length(variables))
+## round(phi, 4)
